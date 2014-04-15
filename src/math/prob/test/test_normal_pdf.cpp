517faaa22b222a1bc90df6d802729ec98ae87680
Keith A. Lewis <keithalewis@live.com>
2014-04-15 12:02:40 -0400

    simple data target for generating cdf/pdf tests

---
+++ src/math/prob/test/test_normal_pdf.cpp
@@ -0,0 +1,25 @@
+// test_normal_pdf.cpp - test normal::pdf
+#include <limits>
+#include "../normal.h"
+
+template<class X>
+void test_normal_pdf(void)
+{
+	X y;
+	X eps = std::numeric_limits<X>::epsilon();
+	prob::normal<X> n;
+
+	struct { X x, y; } test_normal_pdf_dat[] = {
+#include "test_normal_pdf.dat"
+	};
+	for (auto xy : test_normal_pdf_dat) {
+		y = n.pdf(xy.x);
+		ensure (static_cast<X>(fabs(y - xy.y) < eps));
+	}
+}
+
+void test_normal_pdf(void)
+{
+	test_normal_pdf<float>();
+	test_normal_pdf<double>();
+}
